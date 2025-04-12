0618857eb82632dfbd034544a28df5c69b131fc8
nathanbuyr <G00421082@atu.ie>
2025-04-13 00:13:59 +0100

    Added pages

---
+++ scorefactor-app/src/app/components/score-display/score-display.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-score-display',
+  imports: [],
+  templateUrl: './score-display.component.html',
+  styleUrl: './score-display.component.css'
+})
+export class ScoreDisplayComponent {
+
+}
