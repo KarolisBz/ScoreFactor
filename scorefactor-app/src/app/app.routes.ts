b8a017c0241ed399f21b8f274b6cec9b82df4657
nathanbuyr <G00421082@atu.ie>
2025-04-13 00:22:05 +0100

    Added incrementing score counter on the home page

--- scorefactor-app/src/app/app.routes.ts
+++ scorefactor-app/src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { LeaderboardComponent } from './pages/leaderboard/leaderboard.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'leaderboard', component: LeaderboardComponent },
+];
+
--- scorefactor-app/src/app/pages/home/home.component.ts
+++ scorefactor-app/src/app/pages/home/home.component.ts
@@ -1,11 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home',
-  imports: [],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  username = 'Karolis'; // Change this later to be dynamic
+  score = 0;
+  currentTime: string = '';
 
+  ngOnInit(): void {
+    this.updateTime();
+    setInterval(() => this.updateTime(), 1000);
+
+    // Simulate score updates (replace with socket later)
+    setInterval(() => {
+      this.score += Math.floor(Math.random() * 5); // random test logic
+    }, 5000);
+  }
+
+  updateTime(): void {
+    this.currentTime = new Date().toLocaleString();
+  }
 }
