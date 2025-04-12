36a285b86a08609e95ca38344da143049ae2f905
nathanbuyr <G00421082@atu.ie>
2025-04-13 00:36:49 +0100

    Added Universal Navbar

--- scorefactor-app/src/app/app.routes.ts
+++ scorefactor-app/src/app/app.routes.ts
@@ -1,9 +1,16 @@
-import { Routes } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { LeaderboardComponent } from './pages/leaderboard/leaderboard.component';
+import { NgModule } from '@angular/core';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'leaderboard', component: LeaderboardComponent },
 ];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule {}
 
--- scorefactor-app/src/app/pages/leaderboard/leaderboard.component.ts
+++ scorefactor-app/src/app/pages/leaderboard/leaderboard.component.ts
@@ -1,11 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+interface LeaderboardEntry {
+  username: string;
+  score: number;
+}
 
 @Component({
   selector: 'app-leaderboard',
-  imports: [],
   templateUrl: './leaderboard.component.html',
-  styleUrl: './leaderboard.component.css'
+  styleUrls: ['./leaderboard.component.css']
 })
-export class LeaderboardComponent {
+export class LeaderboardComponent implements OnInit {
+  leaderboard: LeaderboardEntry[] = [];
 
+  ngOnInit(): void {
+    this.generateDummyData();
+  }
+
+  generateDummyData() {
+    const usernames = ['Karolis', 'Zoe', 'Max', 'Liam', 'Ava', 'Noah', 'Mia', 'Lucas', 'Olivia', 'Jack'];
+
+    this.leaderboard = usernames.map(username => ({
+      username,
+      score: Math.floor(Math.random() * 100)
+    }))
+    .sort((a, b) => b.score - a.score); // Sort high to low
+  }
 }
