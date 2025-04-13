cd9dd93bfae7ac8a5b9b4cb1c362f4fb071d1b7d
Karolis Bandziulis <G00417529@atu.ie>
2025-04-13 13:39:26 +0100

    testing multifile C file architecture

---
+++ pi-controller/add.c
@@ -0,0 +1,4 @@
+// testing C multi files for final architecture
+int sum(int a, int b) {
+    return a + b;
+}
---
+++ pi-controller/main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    printf("Welcome to Score Factor!\n");
+    // Your code logic here
+
+    int sum = add(2, 3); // Example function call
+    printf("Sum: %d\n", sum); // Example output
+
+    // exit successfully
+    return 0;
+}
