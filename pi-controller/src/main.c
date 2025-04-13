4005e03fe80cdcac1e6bd15ccfbbf6983b33d612
Karolis Bandziulis <G00417529@atu.ie>
2025-04-13 22:09:26 +0100

    Created Score, handling all sensor pins

--- pi-controller/src/main.c
+++ pi-controller/src/main.c
@@ -4,11 +4,19 @@
 #include <pigpio.h>
 #include "vibration_sensors.h"
 
-#define VIBRATION_PIN 20  // GPIO20 = Pin 38
+// BCM pin numbering, all vibration pins
+#define VIBRATION_PIN_S5 20  // Pin 38 on pi GPIO header (score 5)
+#define VIBRATION_PIN_S4 13  // Pin 33 on pi GPIO header (score 4)
+#define VIBRATION_PIN_S3 16  // Pin 36 on pi GPIO header (score 3)
+#define VIBRATION_PIN_S2 19  // Pin 35 on pi GPIO header (score 2)
+#define VIBRATION_PIN_S1 26  // Pin 37 on pi GPIO header (score 1)
 
 int main() {
+    // initalizing variables
+    int total_score = 0;
+
+    // printing welcome message
     printf("Welcome to Score Factor!\n");
-    // Your code logic here
 
     int sum = add(2, 3); // Example function call
     printf("Sum: %d\n", sum); // Example output
@@ -20,7 +28,11 @@ int main() {
     }
 
     // intilizing vibration pins
-    vibration_init(VIBRATION_PIN);
+    vibration_init(VIBRATION_PIN_S5, &total_score, 5);
+    vibration_init(VIBRATION_PIN_S4, &total_score, 4);
+    vibration_init(VIBRATION_PIN_S3, &total_score, 3);
+    vibration_init(VIBRATION_PIN_S2, &total_score, 2);
+    vibration_init(VIBRATION_PIN_S1, &total_score, 1);
 
     printf("System running. Press Ctrl+C to stop.\n");
     while (1) {
