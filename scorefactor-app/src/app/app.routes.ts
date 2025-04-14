7b67da3d37af66dcbeb66371a32267b58533ae17
nathanbuyr <G00421082@atu.ie>
2025-04-14 02:35:59 +0100

    Added login and logout feature

--- scorefactor-app/src/app/app.routes.ts
+++ scorefactor-app/src/app/app.routes.ts
@@ -1,16 +1,20 @@
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './pages/login/login.component';
 import { HomeComponent } from './pages/home/home.component';
 import { LeaderboardComponent } from './pages/leaderboard/leaderboard.component';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
-export const routes: Routes = [
-  { path: '', component: HomeComponent },
+export const routes:Routes = [
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  { path: 'login', component: LoginComponent },
+  { path: 'home', component: HomeComponent },
   { path: 'leaderboard', component: LeaderboardComponent },
 ];
-
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  imports: [RouterModule.forRoot(routes), FormsModule],
+  exports: [RouterModule, FormsModule]
 })
 export class AppRoutingModule {}
 
+ 
